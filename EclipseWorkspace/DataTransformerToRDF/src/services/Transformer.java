4ddd93a88f8a5070cefe7ca6c4eb97156f9ba049
Quentin <quentin.ceschin@gmail.com>
2021-05-22 22:19:17 +0200

    finish app; update alignement; add ttl file

--- EclipseWorkspace/DataTransformerToRDF/src/services/Transformer.java
+++ EclipseWorkspace/DataTransformerToRDF/src/services/Transformer.java
@@ -2,7 +2,9 @@ package services;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -211,21 +213,30 @@ public class Transformer {
 	
 	public void doAlignStreet() {
 		for (Lieu l : dictLieux.values()) {
-			String request = 
-					"PREFIX dbo: <http://dbpedia.org/ontology/>\r\n"
-					+ "PREFIX geo:  <http://www.w3.org/2003/01/geo/wgs84_pos#>\r\n"
-					+ "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n"
-					+ "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\r\n"
-					+ "SELECT * WHERE {\r\n"
-					+ "?uriRessoures a dbo:Place;\r\n"
-					+ "rdfs:label \""+ l.adresse +"@fr.\r\n"
-					+ "}";
+			String request = "PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>\r\n" + 
+					"prefix dbpedia: <http://dbpedia.org/resource/>\r\n" + 
+					"prefix sc: <http://schema.org/>\r\n" + 
+					"prefix w3geo: <http://www.w3.org/2003/01/geo/wgs84_pos#>\r\n" + 
+					"\r\n" + 
+					"select ?lieu where {\r\n" + 
+					"service <http://dbpedia.org/sparql/> {	\r\n" + 
+					"	\r\n" + 
+					"?lieu rdfs:label ?adr.\r\n" + 
+					"?lieu a sc:Place.\r\n" + 
+					"    filter contains(?adr, \""+ l.adresse+"\").\r\n" + 
+					"      filter langMatches(lang(?adr),'fr').\r\n" + 
+					"    \r\n" + 
+					"}\r\n" + 
+					"}";
 			
-			//Iterable<Map<String, String>> response = dbpediaClient.select(request);
-			
-			String uri = "customUri";
-			
-			l.dbpediaURI = uri;
+			Iterable<Map<String, String>> response = sparqlClient.select(request);
+			String uri = null;
+			for (Map<String, String> map : response) {
+				uri = map.get("lieu");
+			}
+			if (uri != null) {
+				l.dbpediaURI = uri;				
+			}
 		}
 	}
 
